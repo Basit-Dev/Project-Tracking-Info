78c9c605ad838eef4fdf8a503d976e542871056a
pivanta <bas677@icloud.com>
2020-11-12 20:16:26 +0000

    Fixed mobile menu error

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
 /* When the user clicks on the button, #foot between hiding and showing the dropdown content */
 
 function myFunction() {
-
-    
     document.getElementById("myDropdown").classList.toggle("show");
     var element = document.getElementById("body");
-    element.classList.toggle("toggle_cta"); 
-
-
+    element.classList.toggle("toggle_cta");
 }  
-  // Close the dropdown if the user clicks outside of it 
